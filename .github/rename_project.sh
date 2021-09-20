83895ac87306ff0fae6487526c46918f1dff8c44
tzengwei <tzengwei@users.noreply.github.com>
2021-09-20 01:31:57 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="tzengwei"
+original_name="fastapi_sqlmodel_typer"
+original_urlname="fastapi-sqlmodel-typer"
+original_description="Awesome fastapi_sqlmodel_typer created by tzengwei"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_sqlmodel_typer $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- fastapi_sqlmodel_typer/app.py
+++ fastapi_sqlmodel_typer/app.py
@@ -20,18 +20,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+fastapi_sqlmodel_typer API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="fastapi_sqlmodel_typer",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://fastapi_sqlmodel_typer.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "tzengwei",
+        "url": "http://fastapi_sqlmodel_typer.com/contact/",
+        "email": "tzengwei@fastapi_sqlmodel_typer.com",
     },
     license_info={
         "name": "The Unlicense",
